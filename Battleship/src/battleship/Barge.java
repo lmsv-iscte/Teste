528ac849a8e03dd85acca374cb2121e80eb7b47e
Luis Viriato <lmsvo@iscte-iul.pt>
2023-02-14 22:34:38 +0000

    Initial commit

--- Battleship/src/battleship/Barge.java
+++ Battleship/src/battleship/Barge.java
@@ -9,24 +9,24 @@ package battleship;
  */
 public class Barge extends Ship {
 
-    private static final int SIZE = 1;
-    private static final String NAME = "Barca";
+	private static final int SIZE = 1;
+	private static final String NAME = "Barca";
 
-    /**
-     * @param bearing - barge bearing
-     * @param pos     - upper left position of the barge
-     */
-    public Barge(Compass bearing, IPosition pos) throws IllegalArgumentException {
-	super(Barge.NAME, bearing, pos);
-	if (bearing == null || pos == null)
-	    throw new IllegalArgumentException("ERROR! invalid bearing for the barge");
-	else
-	    positions.add(new Position(pos.getRow(), pos.getColumn()));
-    }
+	/**
+	 * @param bearing - barge bearing
+	 * @param pos     - upper left position of the barge
+	 */
+	public Barge(Compass bearing, IPosition pos) throws IllegalArgumentException {
+		super(Barge.NAME, bearing, pos);
+		if (bearing == null || pos == null)
+			throw new IllegalArgumentException("ERROR! invalid bearing for the barge");
+		else
+			positions.add(new Position(pos.getRow(), pos.getColumn()));
+	}
 
-    @Override
-    public int getSize() {
-	return SIZE;
-    }
+	@Override
+	public int getSize() {
+		return SIZE;
+	}
 
 }
--- Battleship/src/battleship/Carrack.java
+++ Battleship/src/battleship/Carrack.java
@@ -8,7 +8,7 @@ package battleship;
  *
  */
 public class Carrack extends Ship {
-	
+
 	private static final int SIZE = 3;
 	private static final String NAME = "Nau";
 
@@ -20,16 +20,18 @@ public class Carrack extends Ship {
 		super(Carrack.NAME, bearing, pos);
 		if (bearing == Compass.NORTH || bearing == Compass.SOUTH)
 			for (int r = 0; r < SIZE; r++)
-				positions.add(new Position(pos.getRow()+r, pos.getColumn()));
+				positions.add(new Position(pos.getRow() + r, pos.getColumn()));
 		else if (bearing == Compass.EAST || bearing == Compass.WEST)
 			for (int c = 0; c < SIZE; c++)
-				positions.add(new Position(pos.getRow(), pos.getColumn()+c));
+				positions.add(new Position(pos.getRow(), pos.getColumn() + c));
 		// else ... TODO: invalid bearing
-		else 
+		else
 			throw new IllegalArgumentException("ERROR! invalid bearing for the carrack");
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see battleship.Ship#getSize()
 	 */
 	@Override
